6d4b82d3bf8d0432a7754142ce503800bff6ffa8
Lukasz Dudek <lukasz.dudek@silvair.com>
2019-08-11 21:47:36 +0200

    feat(k-primes)[5]: add broken prime factors getting

--- src/kyu5/k_primes.rs
+++ src/kyu5/k_primes.rs
@@ -1,31 +1,45 @@
 use std::ops::RangeInclusive;
 
-fn factors(n: i32) -> Vec<i32> {
+
+fn get_primes_up_to(n: i32) -> Vec<i32> {
+    if n < 2 {
+        return vec![];
+    }
+    let mut is_prime = vec![true; n as usize + 1];
+    (0..=1).for_each(|i| is_prime[i] = false);
     get_to_square_range(n)
-        .filter(|&i| n % i == 0)
+        .filter(|&i| is_prime[i as usize])
+        .for_each(|i| (i * i..n).step_by(i as usize)
+            .for_each(|j| is_prime[j as usize] = false)
+        );
+    is_prime.iter()
+        .enumerate()
+        .filter(|(_, &is_prime)| is_prime)
+        .map(|(p, _)| p as i32)
         .collect()
 }
 
-fn is_prime(n: i32) -> bool {
-    get_to_square_range(n)
-        .filter(|&i| n % i == 0)
-        .last().is_none()
+fn is_k_prime(n: i32, k: i32, primes: &Vec<i32>) -> bool {
+    let mut counter = 0;
+    let mut part_of_n = n;
+    primes.iter()
+        .for_each(|&i| {
+            while part_of_n % i as i32 == 0 {
+                part_of_n /= i as i32;
+                counter += 1;
+            }
+        });
+    counter == k
 }
-
 
 fn get_to_square_range(n: i32) -> RangeInclusive<i32> {
     (2..=(n as f32).sqrt().floor() as i32)
 }
 
-fn prime_factors<'a>(n: i32) -> Vec<i32> {
-    factors(n).into_iter()
-        .filter(|&i| is_prime(i))
-        .collect()
-}
-
 fn count_kprimes(k: i32, start: i32, nd: i32) -> Vec<i32> {
+    let primes = get_primes_up_to(nd);
     (start..=nd)
-        .filter(|&n| prime_factors(n).len() == k as usize)
+        .filter(|&n| is_k_prime(n, k, &primes))
         .collect()
 }
 
@@ -53,4 +67,11 @@ fn basics_puzzle() {
     testing(100, 0);
     testing(144, 0);
     testing(143, 2);
+}
+
+#[test]
+fn test_eratosthenes_sieve() {
+    assert_eq!(get_primes_up_to(2), vec![]);
+    assert_eq!(get_primes_up_to(10), vec![2, 3, 5, 7]);
+    assert_eq!(get_primes_up_to(20), vec![2, 3, 5, 7, 11, 13, 17, 19]);
 }
