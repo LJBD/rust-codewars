267199171dfc8a1deaf898e79d8d929dbefbe86d
Lukasz Dudek <lukasz.dudek@silvair.com>
2019-08-15 21:07:33 +0200

    feat(k-primes)[5]: add solution passing basic tests

--- src/kyu5/k_primes.rs
+++ src/kyu5/k_primes.rs
@@ -7,11 +7,13 @@ fn get_primes_up_to(n: i32) -> Vec<i32> {
     }
     let mut is_prime = vec![true; n as usize + 1];
     (0..=1).for_each(|i| is_prime[i] = false);
-    get_to_square_range(n)
-        .filter(|&i| is_prime[i as usize])
-        .for_each(|i| (i * i..n).step_by(i as usize)
-            .for_each(|j| is_prime[j as usize] = false)
-        );
+    for i in get_to_square_range(n) {
+        if is_prime[i as usize] {
+            for j in (i * i..=n).step_by(i as usize) {
+                is_prime[j as usize] = false;
+            }
+        }
+    }
     is_prime.iter()
         .enumerate()
         .filter(|(_, &is_prime)| is_prime)
@@ -27,6 +29,9 @@ fn is_k_prime(n: i32, k: i32, primes: &Vec<i32>) -> bool {
             while part_of_n % i as i32 == 0 {
                 part_of_n /= i as i32;
                 counter += 1;
+                if counter > k {
+                    break;
+                }
             }
         });
     counter == k
@@ -44,8 +49,20 @@ fn count_kprimes(k: i32, start: i32, nd: i32) -> Vec<i32> {
 }
 
 fn puzzle(s: i32) -> i32 {
-    // your code
-    s
+    let primes = get_primes_up_to(s);
+    let three_primes = count_kprimes(3, 0, s);
+    let seven_primes = count_kprimes(7, 0, s);
+    let mut counter = 0;
+    for i in &seven_primes {
+        for j in &three_primes {
+            for k in &primes {
+                if *i + *j + *k == s {
+                    counter += 1;
+                }
+            }
+        }
+    }
+    counter
 }
 
 fn testing_count_kprimes(k: i32, start: i32, nd: i32, exp: Vec<i32>) -> () {
@@ -71,7 +88,7 @@ fn basics_puzzle() {
 
 #[test]
 fn test_eratosthenes_sieve() {
-    assert_eq!(get_primes_up_to(2), vec![]);
+    assert_eq!(get_primes_up_to(2), vec![2]);
     assert_eq!(get_primes_up_to(10), vec![2, 3, 5, 7]);
     assert_eq!(get_primes_up_to(20), vec![2, 3, 5, 7, 11, 13, 17, 19]);
 }
